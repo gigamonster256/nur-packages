6dbbe79bfb9ddb1d26a1e4f4941b4a442482ea51
Caleb Norton <n0603919@outlook.com>
2024-06-24 18:01:37 -0500

    Update flake.nix
    Add cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  nixConfig = {
+    extra-substituters = [ "https://gigamonster256.cachix.org" ];
+    extra-trusted-public-keys = [ "gigamonster256.cachix.org-1:ySCUrOkKSOPm+UTipqGtGH63zybcjxr/Wx0UabASvRc=" ];
+  };
   outputs = { self, nixpkgs }:
     let
       systems = [
